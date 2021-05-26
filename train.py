6b4b00f96e5902d6c4e3205d353e1397150370df
taekwang94 <58813265+taekwang94@users.noreply.github.com>
2021-05-26 10:20:08 +0900

    Update train.py
    Early stop, Lr scheduler

--- train.py
+++ train.py
@@ -27,8 +27,11 @@ from models.shufflenetv2 import shufflenet_v2_x1_0
 from models.mobilenetv3 import mobilenetv3_small
 from models.ghostnet import ghostnet
 from models.mnasnet import mnasnet1_0
-
+from models.mobilenetv2_ch_in_model import mobilenet_v2_in_ch
 from timeit import default_timer as timer
+
+from tools.tool import EarlyStopping
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
 
 # Multi-GPUs
 from torch.nn.parallel import DistributedDataParallel as DDP
@@ -42,19 +45,53 @@ print('학습을 진행하는 기기:', device)
 
 def main():
     parser = argparse.ArgumentParser(description='FireClassification')
-    parser.add_argument('--model', default='mobilenet_v2')  # resnet18, mobilenet_v2, sh
+    parser.add_argument('--model', default='ghostnet')  # resnet18, mobilenet_v2, sh
     parser.add_argument('--train_save_path', default='/home/taekwang0094/WorkSpace/FireTraining')
     parser.add_argument('--multi_gpus', default=True)
     parser.add_argument('--root', default='/home/taekwang0094/WorkSpace/Summer_Conference')
-    parser.add_argument('--channel_multiplier', default=True)  # -l 추가해서 list로 받도록 수정할것
-    parser.add_argument('--batch_size', default=128)
+    parser.add_argument('--channel_multiplier', default=[1.0,1.0,1.0])  # -l 추가해서 list로 받도록 수정할것
+    parser.add_argument('--batch_size', default=258)
     parser.add_argument('--epoch', default=100)
+    parser.add_argument('--early_stop', default=True)
+    parser.add_argument('--lr_scheduler', default=True)
 
     args = parser.parse_args()
     print(args.model)
 
-    channel_multiplier = [1, 2, 1]
+    if type(args.channel_multiplier) == str:
+        tmp = args.channel_multiplier
+        tmp = tmp.replace('[',"")
+        tmp = tmp.replace(']',"")
+        tmp = tmp.split(',')
+        channel_multiplier = [float(tmp[0]),float(tmp[1]),float(tmp[2])]
+    else:
+        channel_multiplier = args.channel_multiplier
 
+    if type(args.multi_gpus) ==str:
+        if args.multi_gpus == 'True':
+            multi_gpus = True
+        else:
+            multi_gpus = False
+    else:
+        multi_gpus = args.multi_gpus
+
+    if type(args.early_stop) == str:
+        if args.early_stop == 'True':
+            is_early_stop = True
+        else:
+            False
+    else:
+        is_early_stop = args.early_stop
+
+    if type(args.lr_scheduler) == str:
+        if args.lr_scheduler == 'True':
+            is_lr_scheduler = True
+        else:
+            False
+    else:
+        is_lr_scheduler = args.lr_scheduler
+
+    print("Multi gpus :" , multi_gpus)
     data_transforms = {
         'train': transforms.Compose([
             # transforms.RandomResizedCrop(224),
@@ -73,17 +110,17 @@ def main():
         ]),
     }
     train_loader = torch.utils.data.DataLoader(
-        dataset.FireDataset(args.root, transforms=data_transforms['train'], channel_multiplier=channel_multiplier),
+        dataset.FireDataset(args.root, transforms=data_transforms['train'], channel_multiplier=channel_multiplier, ch_preprocess=True),
         batch_size=int(args.batch_size),
         shuffle=True,
-        num_workers=4
+        num_workers=8
     )
     val_loader = torch.utils.data.DataLoader(
         dataset.FireDataset(args.root, train='val', transforms=data_transforms['val'],
-                            channel_multiplier=channel_multiplier),
-        batch_size=1,
+                            channel_multiplier=channel_multiplier, ch_preprocess=True),
+        batch_size=128,
         shuffle=False,
-        num_workers=4
+        num_workers=8
     )
     if args.model == 'resnet18':
         model = resnet18()
@@ -103,23 +140,35 @@ def main():
         model = ghostnet(num_classes=2)
     elif args.model == 'mnasnet1_0':
         model = mnasnet1_0(num_classes=2)
+    elif args.model == 'mobilenet_v2_in_ch':
+        model = mobilenet_v2_in_ch(num_classes = 2)
 
     print("Training Start , model : ", args.model)
 
-    save_dir_path = os.path.join(args.train_save_path, args.model)
+    save_dir = args.model + "_[{0},{1},{2}]_2".format(channel_multiplier[0], channel_multiplier[1], channel_multiplier[2])
+    # save_dir = os.path.join(args.model,'_[{0},{1},{2}]'.format(channel_multiplier[0],channel_multiplier[1],channel_multiplier[2]))
+    save_dir_path = os.path.join(args.train_save_path, save_dir)
+    #save_dir_path = os.path.join(args.train_save_path, args.model)
     if not os.path.exists(save_dir_path):
         os.makedirs(save_dir_path)
 
-    if args.multi_gpus:
+    if multi_gpus:
         model = torch.nn.DataParallel(model)
         model.cuda()
+
     else:
-        model = model.to(device)
+        #model = model.to(device)
+        model = model.cuda()
 
     if args.model == 'mnasnet1_0':
         optimizer = optim.SGD(model.parameters(), lr=0.003)
     else:
-        optimizer = optim.SGD(model.parameters(), lr=0.001)
+        optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    #LR scheduler
+    if is_lr_scheduler:
+        lr_scheduler = ReduceLROnPlateau(optimizer, mode = 'min', factor=0.5, patience=5, verbose=1)
+
     criterion = torch.nn.BCELoss().to(device)
 
     best_val_loss = sys.maxsize
@@ -129,7 +178,20 @@ def main():
     time_1 = 0
     time_2 = 0
 
-    for epochs in range(args.epoch):
+    # Early Stop
+    if is_early_stop:
+        early_stopping = EarlyStopping(patience=7, verbose=True)
+
+    # 모델이 학습되는 동안 trainning loss를 track
+    train_losses = []
+    # 모델이 학습되는 동안 validation loss를 track
+    valid_losses = []
+    # epoch당 average training loss를 track
+    avg_train_losses = []
+    # epoch당 average validation loss를 track
+    avg_valid_losses = []
+
+    for epochs in range(int(args.epoch)):
         model.train()
 
         train_correct = 0
@@ -150,12 +212,16 @@ def main():
             cost.backward()
             optimizer.step()
             # print(output)
+
+            train_losses.append(cost.item())
+
             _, predicted = torch.max(output.data, 1)
             # print("ASD",predicted)
             # print(torch.argmax(label,1))
             train_label_eval = torch.argmax(label, 1)
             train_total += label.size(0)
             train_correct += (predicted == train_label_eval).sum().item()
+
             print('{0}% 완료,\r'.format(int(batch_idx / len(train_loader) * 100)), end="")
 
         torch.save({
@@ -173,6 +239,7 @@ def main():
             with torch.no_grad():
                 output = model(image).to(device)
                 val_loss = criterion(output, label)
+                valid_losses.append(val_loss.item())
                 _, predicted = torch.max(output.data, 1)
                 val_label_eval = torch.argmax(label, 1)
                 val_total += label.size(0)
@@ -191,6 +258,20 @@ def main():
         train_accuracy = round(float(100 * train_correct / train_total), 4)
         val_accuracy = round(float(100 * val_correct / val_total), 4)
 
+        train_loss = np.average(train_losses)
+        valid_loss = np.average(valid_losses)
+        avg_train_losses.append(train_loss)
+        avg_valid_losses.append(valid_loss)
+
+        train_losses = []
+        valid_losses = []
+
+        if is_early_stop:
+            early_stopping(valid_loss, model)
+            if early_stopping.early_stop:
+                print("Early stopping")
+                break
+
         print("Epoch : ", epochs, " Training Loss : ", round(cost.item(), 4), " Training Accuracy : ", train_accuracy,
               " Validation Loss : ", round(val_loss.item(), 4), " Validation Accuracy : ", val_accuracy)
         word = "Epoch : {0} Training Loss : {1} Training Accuracy {2} Validation Loss : {3} Validation Accuracy {4} \n".format(
@@ -198,10 +279,11 @@ def main():
         file_txt.write(word)
         time_2 = timer()
         print("Train time / epoch : ", time_2 - time_1)
+        if is_lr_scheduler:
+            lr_scheduler.step(val_loss)
 
     file_txt.close()
 
 
 if __name__ == "__main__":
     main()
-
